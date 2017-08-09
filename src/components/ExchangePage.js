84d867b3232fd56f4865e12d8ddb5f2943b3ecca
Sum Ling <sumling@isuntv-hk.com>
2017-08-09 17:07:31 +0800

    Show exchange history on Exchange Page

--- src/components/ExchangePage.js
+++ src/components/ExchangePage.js
@@ -1,10 +1,144 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Modal from 'react-modal';
+import moment from 'moment';
 import AsyncButton from './common/AsyncButton';
 import { VaultClient, TidePayAPI, VCUtils as Utils } from '../logics';
 import UnlockButton from './common/UnlockButton';
 import DropdownMenu from './common/DropdownMenu';
 import AccountBalanceTable from './common/AccountBalanceTable';
+
+function TransactionHistory(props) {
+  const {
+    exchangeHistory,
+    onSelectExchange,
+  } = props;
+
+  if (!exchangeHistory) {
+    return (
+      <div>
+        Loading...
+      </div>
+    );
+  }
+
+  function convertHuman(transaction) {
+    const {
+      time,
+      status,
+      currency,
+      amount,
+      targetCurrency,
+      targetAmount,
+    } = transaction;
+
+    const statusText = ['Processing', 'Failed', 'Success'];
+    return {
+      time: moment.unix(time).format('D/M/YY h:mm A'),
+      descriptions: [
+        `From ${amount}${currency}`,
+        `To ${targetAmount}${targetCurrency}`,
+      ],
+      status: statusText[status + 1],
+    };
+  }
+
+  function handleSelectExchange(transactionID, event) {
+    event.preventDefault();
+    onSelectExchange(transactionID);
+  }
+
+  const rows = exchangeHistory.map((transaction) => {
+    const {
+      transactionID,
+    } = transaction;
+    const {
+      time,
+      descriptions,
+      status,
+    } = convertHuman(transaction);
+    const onClick = handleSelectExchange.bind(this, transactionID);
+    return (
+      <tr key={transactionID}>
+        <td>{time}</td>
+        <td>{descriptions[0]}<br />{descriptions[1]}</td>
+        <td>{status}</td>
+        <td>
+          <button onClick={onClick}>Details</button>
+        </td>
+      </tr>
+    );
+  });
+
+  if (rows.length === 0) {
+    return (
+      <div>
+        No history!
+      </div>
+    );
+  }
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td width="200">Date</td>
+          <td width="350">Description</td>
+          <td width="100">Status</td>
+          <td width="100">&nbsp;</td>
+        </tr>
+      </thead>
+      <tbody>{rows}</tbody>
+    </table>
+  );
+}
+
+function TransactionModal(props) {
+  const {
+    selectedExchange,
+    exchangeDetails,
+    onClose,
+  } = props;
+
+  if (!selectedExchange) {
+    return null;
+  }
+
+  let content;
+  if (exchangeDetails) {
+    const {
+      transactionID,
+      ...rest
+    } = exchangeDetails;
+    content = (
+      <div>
+        <h1>ID: {transactionID}</h1>
+        <pre>{JSON.stringify(rest, null, 2)}</pre>
+      </div>
+    );
+  } else {
+    content = (
+      <div>
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  const handleClose = (event) => {
+    event.preventDefault();
+    onClose();
+  };
+  return (
+    <Modal
+      isOpen={!!selectedExchange}
+      contentLabel={selectedExchange}
+    >
+      {content}
+      <br />
+      <button onClick={handleClose}>Close</button>
+    </Modal>
+  );
+}
 
 function ExchangeRateForm(props) {
   const {
@@ -120,12 +254,17 @@ export default class ExchangePage extends React.Component {
       exchangeRate: null,
       note: '',
       supportedExchangePairs: {},
+      exchangeHistory: null,
+      selectedExchange: null,
+      exchangeDetails: null,
     };
     this.handleCurrencyChange = this.handleCurrencyChange.bind(this);
     this.handleSubmitExchangeRateForm = this.handleSubmitExchangeRateForm.bind(this);
     this.handleSubmitExchangeForm = this.handleSubmitExchangeForm.bind(this);
     this.onUnlock = this.onUnlock.bind(this);
     this.onGetAccountBalances = this.onGetAccountBalances.bind(this);
+    this.handleSelectExchange = this.handleSelectExchange.bind(this);
+    this.handleExchangeModalClose = this.handleExchangeModalClose.bind(this);
   }
 
   componentDidMount() {
@@ -138,6 +277,27 @@ export default class ExchangePage extends React.Component {
         unlockSecret: blob.data.unlock_secret,
         supportedExchangePairs: exchangePairs,
       });
+      
+      // load exchange history
+      const gatewayAddressPromise = TidePayAPI.getGatewayAddress();
+      const accountExchangesPromise = gatewayAddressPromise
+        .then((value) => {
+          const options = {
+            limit: 10,
+            counterparty: value.gateway,
+          };
+          return TidePayAPI.getAccountExchanges(account_id, options);
+        });
+      const promise = accountExchangesPromise;
+      this.cancelablePromise = Utils.makeCancelable(promise);
+      this.cancelablePromise.promise
+        .then((resp) => {
+          const { result } = resp;
+          const { transactions } = result;
+          this.setState({
+            exchangeHistory: transactions,
+          });
+        });
     };
     const loginInfoPromise = VaultClient.getLoginInfo()
       .catch((err) => {
@@ -248,6 +408,51 @@ export default class ExchangePage extends React.Component {
     this.setState({ [name]: currency });
   }
 
+  handleSelectExchange(transactionID) {
+    if (this.cancelablePromise) {
+      this.cancelablePromise.cancel();
+    }
+    const { exchangeDetails } = this.state;
+    if (exchangeDetails && exchangeDetails.transactionID === transactionID) {
+      this.setState({
+        selectedExchange: transactionID,
+      });
+      return;
+    }
+
+    this.setState({
+      selectedExchange: transactionID,
+      exchangeDetails: null,
+    });
+    const {
+      public: address,
+    } = this.state;
+    const gatewayAddressPromise = TidePayAPI.getGatewayAddress();
+    const transactionDetailPromise = gatewayAddressPromise
+      .then((value) => {
+        const options = {
+          address,
+          counterparty: value.gateway,
+        };
+        return TidePayAPI.getTransactionDetail(transactionID, options);
+      });
+    const promise = transactionDetailPromise;
+    this.cancelablePromise = Utils.makeCancelable(promise);
+    this.cancelablePromise.promise
+      .then((resp) => {
+        const { transaction } = resp;
+        this.setState({
+          exchangeDetails: transaction,
+        });
+      });
+  }
+
+  handleExchangeModalClose() {
+    this.setState({
+      selectedExchange: null,
+    });
+  }
+
   render() {
     let childComponents = null;
     if (this.state.public) {
@@ -257,6 +462,17 @@ export default class ExchangePage extends React.Component {
           <UnlockButton address={this.state.public} secret={this.state.secret} hasPaymentPin={this.state.hasPaymentPin} unlockSecret={this.state.unlockSecret} onUnlock={this.onUnlock} />
           <br />
           <AccountBalanceTable address={this.state.public} onGetAccountBalances={this.onGetAccountBalances} />
+          <br />
+          <h2>Transaction History</h2>
+          <TransactionHistory
+            exchangeHistory={this.state.exchangeHistory}
+            onSelectExchange={this.handleSelectExchange}
+          />
+          <TransactionModal
+            selectedExchange={this.state.selectedExchange}
+            exchangeDetails={this.state.exchangeDetails}
+            onClose={this.handleExchangeModalClose}
+          />
           <br />
           <ExchangeRateForm secret={this.state.secret} self={this} baseCurrency={this.state.baseCurrency} supportedPairs={this.state.supportedExchangePairs} currencies={currencies} />
           <br />
