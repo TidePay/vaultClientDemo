81c8dc7a722c7f938bb6ad27cea58f23b3b44ed8
Sum Ling <sumling@isuntv-hk.com>
2017-08-09 16:16:31 +0800

    Show equivalent USD value in account balance table

--- src/components/common/AccountBalanceTable.js
+++ src/components/common/AccountBalanceTable.js
@@ -11,11 +11,19 @@ export default class AccountBalanceTable extends React.Component {
 
   componentDidMount() {
     const { address } = this.props;
-    const setBalances = (balances) => {
+    const setBalances = ([balances, exchangeRates]) => {
       const balanceObj = balances.lines.reduce((acc, curr) => {
+        const { rates: map } = exchangeRates;
+        const rates = map[curr.currency];
+        const balance = {
+          value: curr.balance,
+        };
+        if (rates) {
+          balance.valueInUSD = curr.balance * rates.USD;
+        }
         return {
           ...acc,
-          [curr.currency]: curr.balance,
+          [curr.currency]: balance,
         };
       }, {});
       this.setState({
@@ -23,7 +31,16 @@ export default class AccountBalanceTable extends React.Component {
       });
       this.props.onGetAccountBalances(balanceObj);
     };
-    const promise = TidePayAPI.getAccountBalances(address);
+    const accountBalancesPromise = TidePayAPI.getAccountBalances(address);
+    const exchangeRatesPromise = accountBalancesPromise
+      .then((balances) => {
+        const bases = balances.lines.map(line => line.currency);
+        return TidePayAPI.getExchangeRate(bases, 'USD');
+      });
+    const promise = Promise.all([
+      accountBalancesPromise,
+      exchangeRatesPromise,
+    ]);
     this.balanceCancelablePromise = Utils.makeCancelable(promise);
     this.balanceCancelablePromise.promise
       .then(setBalances)
@@ -43,10 +60,16 @@ export default class AccountBalanceTable extends React.Component {
     const { balances } = this.state;
     const rows = [];
     Object.keys(balances).forEach((key) => {
+      const balance = balances[key];
+      const {
+        value,
+        valueInUSD = '-',
+      } = balance;
       rows.push(
         <tr key={key}>
           <td>{key}</td>
-          <td>{balances[key]}</td>
+          <td>{value}</td>
+          <td>{valueInUSD}</td>
         </tr>
       );
     });
@@ -63,8 +86,9 @@ export default class AccountBalanceTable extends React.Component {
       <table>
         <thead>
           <tr>
-            <td>Currency</td>
-            <td>Value</td>
+            <td width="100">Currency</td>
+            <td width="200">Value</td>
+            <td width="200">USD Value</td>
           </tr>
         </thead>
         <tbody>{rows}</tbody>
